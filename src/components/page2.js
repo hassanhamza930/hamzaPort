e1dc89f60cefde8d3331ab9a9d9c5efaf8f3ea44
Hamza Hassan <hassanhamza930@gmail.com>
2022-03-02 01:48:23 +0500

    basic changes

--- src/components/page2.js
+++ src/components/page2.js
@@ -7,24 +7,33 @@ const texts = ['Mobile Apps', 'Web Apps', "UI / UX", "Flutter", "Firebase", "Rea
 export default function Page2() {
 
     return (
-        <div className="bg-services bg-center p-10 bg-cyan-900/30 bg-cover backdrop-blur-md bg-center w-full h-[500px] flex flex-col justify-center items-start">
+        <div className="flex flex-row w-full h-screen bg-services bg-center bg-cover">
 
-            <div className="text-white p-4 font-bold rounded-sm text-6xl md:text-[80px] bg-[#084594] text-[#FFD32D]">
-                We do,
+            <div className="bg-gradient-to-br from-[#084594] to-[#084594]/70 p-10 w-full md:w-2/4 h-screen flex flex-col justify-center items-start">
+
+                <div className="text-white p-4 font-bold rounded-sm text-6xl md:text-[80px] text-[#FFD32D]">
+                    We do,
+                </div>
+
+
+
+                <div className="flex justify-start items-center p-2 mt-4 text-4xl text-[#084594] font-normal bg-[#FFD32D]">
+                    <Typewriter
+                        options={{
+                            strings: texts,
+                            autoStart: true,
+                            loop: true,
+                            delay: 80,
+                            deleteSpeed: 50,
+                        }}
+                    />
+                </div>
+
+
             </div>
 
+            <div className="w-0 md:w-2/4 h-screen backdrop-blur-md backdrop-invert">
 
-
-            <div className="flex justify-start items-center p-2 mt-4 text-4xl text-[#084594] font-normal bg-[#FFD32D]">
-                <Typewriter
-                    options={{
-                        strings: texts,
-                        autoStart: true,
-                        loop: true,
-                        delay:80,
-                        deleteSpeed:50,
-                    }}
-                />
             </div>
 
 
